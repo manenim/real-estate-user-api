7b6f6e761982808fd596e24c46e8e3403dcffa86
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-03-09 14:06:14 +0100

    implement automated testing

--- src/lambda/__test__/api.unit.test.js
+++ src/lambda/__test__/api.unit.test.js
@@ -1,7 +1,37 @@
-const { getUserById } = require('../api.js') 
+const { getUserById, register } = require('../api.js') 
 
 
 test('Responses is an object', () => {
     expect(typeof getUserById()).toBe('object')
 })
 
+
+describe('API functions', () => {
+    let mockEvent;
+
+    beforeEach(() => {
+        mockEvent = {
+            body: JSON.stringify({
+                userId: '1234',
+                name: 'Test User',
+                email: 'test@test.com',
+                role: 'renter',
+                propertyId: 'abcd'
+            }),
+            pathParameters: {
+                userId: '1234',
+                propertyId: 'abcd'
+            }
+        };
+    });
+
+    describe('register()', () => {
+        it('should create a user', async () => {
+            const response = await register(mockEvent);
+            expect(response.statusCode).toBe(200);
+            expect(JSON.parse(response.body)).toMatchObject({
+                message: 'Successfully created user.'
+            });
+        });
+    });
+});
